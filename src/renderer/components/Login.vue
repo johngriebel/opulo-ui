8e87e5e4fdf6b589063949226cad42c0966fd5ce
John Griebel <johnkgriebel@gmail.com>
2018-09-02 11:47:07 -0400

    UI-1 - Change logo/icon assets, Add login component.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -1,43 +1,40 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
-  </div>
+    <div class="container is-fluid">
+        <figure class="image is-128x128">
+          <img id="logo" src="~@/assets/logo.png" alt="opulo">
+        </figure>
+            <div class="box">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Username" v-model="username">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="password" placeholder="Password" v-model="password">
+                    </div>
+                </div>
+                <button class="button" v-on:click="login">Log In</button>
+            </div>
+    </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation';
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
     methods: {
-      open(link) {
-        this.$electron.shell.openExternal(link);
+      login() {
+        console.log(this.username);
+        console.log(this.password);
       },
     },
   };
