0c412e814f563f34bbedadf8718635b74445b69a
John Griebel <johnkgriebel@gmail.com>
2018-09-16 20:23:07 -0400

    UI-9 - Add PhotoUpload component and route
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

---
+++ src/renderer/components/PhotoUpload.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <p>This is the photo upload page</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'PhotoUpload',
+    };
+</script>
+
+<style scoped>
+
+</style>
